2e6b85538e3f20e53050fecdf234226b9e79bc81
Michał Mrowiec <michal210101@outlook.com>
2022-08-12 22:52:17 +0200

    Added tests for user email & added total summary & minor fixes

---
+++ src/Core/MoneyManager.Application/Functions/Statistics/Queries/GetCurrentTotalAmount/GetCurrentTotalAmountQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MoneyManager.Application.Functions.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Application.Functions.Statistics.Queries.GetCurrentTotalAmount
+{
+    public class GetCurrentTotalAmountQueryHandler : IRequestHandler<GetCurrentTotalAmountQuery, decimal>
+    {
+        private readonly IMediator _mediator;
+        public GetCurrentTotalAmountQueryHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<decimal> Handle(GetCurrentTotalAmountQuery request, CancellationToken cancellationToken)
+        {
+            var respond = await _mediator.Send(new GetAllRecordsQuery(request.UserId));
+            return respond.Sum(x => x.Amount);
+        }
+    }
+}
--- test/MoneyManager.API.IntegrationTests/ControllerTests/AccountControllerTests.cs
+++ test/MoneyManager.API.IntegrationTests/ControllerTests/AccountControllerTests.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -55,6 +54,29 @@ namespace MoneyManager.API.IntegrationTests.ControllerTests
             new object[] { new RegisterUserCommand { Name = "Test", Email = "test2@test0.com", Password = "qwerty", RepeatPassword = " " } },
             new object[] { new RegisterUserCommand { Name = "Test", Email = "test3est", Password = "qwerty", RepeatPassword = "qwerty" } },
             new object[] { new RegisterUserCommand { Name = "Test above 25 character test", Email = "test4@test0.com", Password = "qwerty", RepeatPassword = "qwerty" } },
+        };
+        public static IEnumerable<object[]> TestRegisterUsers_InvalidModel_SameEmailAddresses => new List<object[]>
+        {
+            new object[]
+            {
+                new RegisterUserCommand { Name = "Test123", Email = "kjsdnfgjndfg@test234.com", Password = "!@#$FDHfdgj45", RepeatPassword = "!@#$FDHfdgj45" },
+                new RegisterUserCommand { Name = "Test123", Email = "kjsdnfgjndfg@test234.com", Password = "!@#$FDHfdgj45", RepeatPassword = "!@#$FDHfdgj45" }
+            },
+            new object[]
+            {
+                new RegisterUserCommand { Name = "123test", Email = "test1op@test.com", Password = "dshdsegdfjn", RepeatPassword = "dshdsegdfjn" },
+                new RegisterUserCommand { Name = "Thomas 12", Email = "test1op@test.com", Password = "47546dfjn", RepeatPassword = "47546dfjn" }
+            },
+            new object[]
+            {
+                new RegisterUserCommand { Name = "Json Carl", Email = "address123a@iop.pl", Password = "da3%2Zg?52", RepeatPassword = "da3%2Zg?52" },
+                new RegisterUserCommand { Name = "Adam von Gen", Email = "address123a@iop.pl", Password = "5hdsHG@$", RepeatPassword = "5hdsHG@$" }
+            },
+            new object[]
+            {
+                new RegisterUserCommand { Name = "@#$TesT", Email = "test4@gmail.com", Password = "34$fsdh#$G", RepeatPassword = "34$fsdh#$G" },
+                new RegisterUserCommand { Name = "@#$TesT", Email = "test4@gmail.com", Password = "34$fsdh#$G", RepeatPassword = "34$fsdh#$G" }
+            }
         };
         public static IEnumerable<object[]> TestLoginUsers_ValidModel => new List<object[]>
         {
@@ -114,6 +136,23 @@ namespace MoneyManager.API.IntegrationTests.ControllerTests
         [MemberData(nameof(TestRegisterUsers_InvalidModel))]
         public async Task CreateUser_WithInvalidModel_ReturnsBadRequest(RegisterUserCommand user)
         {
+            var json = JsonConvert.SerializeObject(user);
+            var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            var respond = await _httpClient.PostAsync("/api/account/register/", httpContent);
+
+            respond.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestRegisterUsers_InvalidModel_SameEmailAddresses))]
+        public async Task CreateUser_WithInvalidModelSameEmailAddresses_ReturnsBadRequest(RegisterUserCommand firstUser, RegisterUserCommand user)
+        {
+            var jsonFU = JsonConvert.SerializeObject(firstUser);
+            var httpContentFU = new StringContent(jsonFU, UnicodeEncoding.UTF8, "application/json");
+            await _httpClient.PostAsync("/api/account/register/", httpContentFU);
+
+
             var json = JsonConvert.SerializeObject(user);
             var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
 
