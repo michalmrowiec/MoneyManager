d0292ddbaf4693934a9707156a74b3ad7f3c434a
Michał Mrowiec <michal210101@outlook.com>
2022-08-12 22:52:17 +0200

    Added tests for user email & added total summary & minor fixes

---
+++ src/API/MoneyManager.BaseAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MoneyManager.API.Services;
+using MoneyManager.Application.Functions.Statistics.Queries.GetCurrentTotalAmount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/stat")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IUserContextService _userContextService;
+        public StatisticsController(IMediator mediator, IUserContextService userContextService)
+        {
+            _mediator = mediator;
+            _userContextService = userContextService;
+        }
+
+        [HttpGet("total")]
+        public async Task<ActionResult<decimal>> GetCurrentTotalAmount()
+        {
+            return Ok(await _mediator.Send(new GetCurrentTotalAmountQuery(_userContextService.GetUserId)));
+        }
+    }
+}
---
+++ src/Core/MoneyManager.Application/Functions/Statistics/Queries/GetCurrentTotalAmount/GetCurrentTotalAmountQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Application.Functions.Statistics.Queries.GetCurrentTotalAmount
+{
+    public record GetCurrentTotalAmountQuery(int UserId) : IRequest<decimal>;
+}
