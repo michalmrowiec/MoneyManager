64a9b62d2a9548f435c0eb87c738954a873eb185
Michał Mrowiec <michal210101@outlook.com>
2022-08-16 12:32:14 +0200

    Fixed response messages in UI
    At login/register page & add new record page

---
+++ src/UI/Client/Models/Response/BaseResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Client.Models.Response
+{
+    public class BaseResponse
+    {
+        public ResponseStatus Status { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public List<string>? ValidationErrors { get; set; }
+
+        public enum ResponseStatus
+        {
+            Success = 0,
+            NotFound = 1,
+            BadQuery = 2,
+            ValidationError = 3
+        }
+    }
+}
