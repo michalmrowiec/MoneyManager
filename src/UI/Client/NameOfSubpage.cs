e95aae04790f4ebe6e5b9cc6cc0b77474af35bf1
Michał Mrowiec <michal210101@outlook.com>
2022-10-24 14:31:13 +0200

    The display of the names of subpages has been changed

---
+++ src/UI/Client/NameOfSubpage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Client
+{
+    public class NameOfSubpage
+    {
+        private NamesOfSubpageEnum _namesOfSubpage;
+        public event Action? OnChange;
+
+        public NamesOfSubpageEnum NamesOfSubpage
+        {
+            get { return _namesOfSubpage; }
+            set
+            {
+                if(_namesOfSubpage != value)
+                _namesOfSubpage = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
+
+public enum NamesOfSubpageEnum
+{
+    [Description("Main Dashboard")]
+    MainDashboard,
+    [Description("Add")]
+    Add,
+    [Description("Financial Dashboard")]
+    FinancialDashboard,
+    [Description("Recurring Records")]
+    RecurringRecords,
+    [Description("Planned Budgets")]
+    PlannedBudgets,
+    [Description("Categories")]
+    Categories,
+    [Description("Settings")]
+    Settings,
+    [Description("")]
+    Empty
+}
