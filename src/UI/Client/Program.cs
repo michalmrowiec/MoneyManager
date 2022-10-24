a0cd6264e6ac51341b28498b202ee019b43ac702
Michał Mrowiec <michal210101@outlook.com>
2022-10-24 14:31:13 +0200

    The display of the names of subpages has been changed

--- src/UI/Client/Program.cs
+++ src/UI/Client/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<AppState>();
+builder.Services.AddSingleton<NameOfSubpage>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ILocalStorageService, LocalStorage>();
 builder.Services.AddScoped<IHttpRecordService, HttpRecordService>();
