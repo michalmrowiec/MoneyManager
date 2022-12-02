9e0a022781b5caec45fa84ce117b56ad3858d3a1
Michał Mrowiec <michalmro210101@gmail.com>
2022-12-02 19:03:34 +0100

    Chart for all category of year & single month <- works

---
+++ src/Core/MoneyManager.Application/Functions/Statistics/Queries/GetTotalsForAllCategoryOfYear/GetTotalsForAllCategoryOfYearQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MoneyManager.Application.Functions.Records;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MoneyManager.Application.Functions.Statistics.Queries.GetTotalsForAllCategoryOfYear
+{
+    public class GetTotalsForAllCategoryOfYearQueryHandler : IRequestHandler<GetTotalsForAllCategoryOfYearQuery, Dictionary<int, decimal>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetTotalsForAllCategoryOfYearQueryHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Dictionary<int, decimal>> Handle(GetTotalsForAllCategoryOfYearQuery request, CancellationToken cancellationToken)
+        {
+            var records = await _mediator.Send(new GetAllRecordsQuery(request.UserId));
+
+            return records.Where(x => x.TransactionDate.Year == request.Year && x.CategoryId != null)
+                .GroupBy(x => (int)x.CategoryId)
+                .ToDictionary(x => x.Key, x => x.Sum(x => x.Amount));
+        }
+    }
+}
--- src/Core/MoneyManager.Application/Functions/Statistics/Queries/GetTotalsForCategoryForEachMonthsOfYear/GetTotalsForCategoryForEachMonthsOfYearQueryHandler.cs
+++ src/Core/MoneyManager.Application/Functions/Statistics/Queries/GetTotalsForCategoryForEachMonthsOfYear/GetTotalsForCategoryForEachMonthsOfYearQueryHandler.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using MoneyManager.Application.Functions.Records;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyManager.Application.Functions.Statistics.Queries.GetTotalsForCategoryForEachMonthsOfYear
 {
