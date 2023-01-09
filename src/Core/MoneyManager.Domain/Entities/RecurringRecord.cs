15066dbe78225d5f2ff9f5e21ddcbd5de656db71
Michał Mrowiec <michalmro210101@gmail.com>
2023-01-09 13:28:14 +0100

    Successfully deployed changes on test database

--- src/Core/MoneyManager.Domain/Entities/RecurringRecord.cs
+++ src/Core/MoneyManager.Domain/Entities/RecurringRecord.cs
@@ -1,27 +1,22 @@
 ﻿using MoneyManager.Domain.Entities.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyManager.Domain.Entities
 {
     public class RecurringRecord : IIdentifier
-	{
-		public int Id { get; set; }
-		public bool IsActive { get; set; }
-		public DateTime NextDate { get; set; }
-		public int RepeatEveryDayOfMonth { get; set; }
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime NextDate { get; set; }
+        public int RepeatEveryDayOfMonth { get; set; }
 
-		public string Name { get; set; } = null!;
-		public decimal Amount { get; set; }
-		public DateTime? TransactionDate { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
 
-		public int? CategoryId { get; set; }
-		public virtual Category? Category { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
 
-		public int UserId { get; set; }
-		public virtual User? User { get; set; }
-	}
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
 }
