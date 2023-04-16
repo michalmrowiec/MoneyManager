b352ec2b01d7552216e451ffa9e25cbb5549042a
Michał Mrowiec <michalmro210101@gmail.com>
2023-04-16 14:25:35 +0200

    Fixed category name in xlxs report

--- src/UI/Client/Services/XlsxFileService/XmlReportGenerateService.cs
+++ src/UI/Client/Services/XlsxFileService/XmlReportGenerateService.cs
@@ -60,7 +60,7 @@ namespace MoneyManager.Client.Services.XlsxFileService
                 row = new();
                 row.Append(
                     new Cell() { CellValue = new CellValue(item.Name), DataType = CellValues.String },
-                    new Cell() { CellValue = new CellValue(item.CategoryName ?? ""), DataType = CellValues.String },
+                    new Cell() { CellValue = new CellValue(item.Category?.Name ?? ""), DataType = CellValues.String },
                     new Cell() { CellValue = new CellValue(item.Amount), DataType = CellValues.Number },
                     new Cell() { CellValue = new CellValue(item.TransactionDate.ToString("dd/MM/yyyy")), DataType = CellValues.String });
                 sheetData.Append(row);
